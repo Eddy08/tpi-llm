632a44ab5b459c5388edb18980903790513781ac
eddy08 <bhardwajharsh08@gmail.com>
2024-10-12 22:02:53 +0530

    Updated pretrained-model.py with login credentails access with token set at run time

--- pretrained-model.py
+++ pretrained-model.py
@@ -1,6 +1,19 @@
-from transformers import AutoModelForCausalLM
+import transformers
+import os
 
+
+# Set token from environment variable
+token = os.environ.get("HUGGINGFACE_HUB_TOKEN")
+
+
+# Login to Hugging Face Hub
+transformers.huggingface_hub.login(token)
+
+
+# Rest of your code...
 model_name = "meta-llama/Llama-2-7b-hf"
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = transformers.AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=True)
 
+
+# Save model locally (optional)
 model.save_pretrained(f"pretrained_models/{model_name}")
